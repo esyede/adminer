716b35c22c7151719814c32be3d3f6edf559afae
esyede <suyadi.1992@gmail.com>
2022-12-02 05:45:50 +0700

    Initial commit

--- routes.php
+++ routes.php
@@ -13,5 +13,24 @@ defined('DS') or exit('No direct script access.');
 |
 */
 
-Route::any('(:package)', 'adminer::home@index');
-Route::any('(:package)/(:any?)', 'adminer::home@index');
+Route::any('(:package)/(:any?)', ['before' => config('adminer::main.middlewares', []), function () {
+    // Disabled
+    abort_if(! config('adminer::main.enabled'), 404);
+
+    // Autologin
+    $db = config('database.connections.'.config('database.default'));
+    $driver = (is_null($db['driver']) || $db['driver'] === 'mysql') ? 'server' : $db['driver'];
+
+    if (! isset($_GET['db'])) {
+        $_POST['auth']['driver'] = $driver;
+        $_POST['auth']['server'] = (isset($db['host']) ? $db['host'] : '')
+            .(isset($db['port']) ? ':'.$db['port'] : '');
+        $_POST['auth']['db'] = ($driver === 'sqlite')
+            ? path('storage').'database'.DS.RAKIT_KEY.'-'.$db['database']
+            : $db['database'];
+        $_POST['auth']['username'] = isset($db['username']) ? $db['username'] : '';
+        $_POST['auth']['password'] = isset($db['password']) ? $db['password'] : '';
+    }
+
+    return require __DIR__.DS.'libraries'.DS.'load.php';
+}]);
